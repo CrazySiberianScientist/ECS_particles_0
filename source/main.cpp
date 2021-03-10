f9cf833a4d14fc2dfdd82e2924eb13b28004948c
moroz <moroz>
2021-03-10 21:36:28 +0700

    Modified   source/main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,44 +1,9 @@
 #include "engine/Engine.h"
-#include <iostream>
-
-
-using namespace std;
-//using namespace UserLogic;
-//using namespace Common;
 
 int main()
 {
 	Common::Engine engine;
-
-	//constexpr int v = Common::Engine::SystemInfo<UserLogic::TestLogicSystem>::destroy_methods_number;
-	//constexpr auto v = Common::Engine::has_destroy<UserLogic::TestLogicSystem, UserLogic::SystemsOrders::Destroy::TEST_0>::value;
-	//printf("!! %d\n", v);
-
-	//return 0;
-
-	auto entity = engine.createEntity();
-	engine.linkEntityToSystem<UserLogic::TestLogicSystem>(entity);
-	/*for (auto i = 0; i < 100; ++i)
-	{
-		auto entity = engine.createEntity();
-		engine.linkEntityToSystem<UserLogic::TestLogicSystem>(entity);
-	}*/
-
-	engine.linkEntityToSystem<EngineLogic::AppSystem>(entity);
-	//engine.unlinkEntityFromSystem<EngineLogic::AppSystem>(entity);
-
-
 	engine.run();
-
-	/*engine.getSystem<CameraSystem>();
-
-	auto e = engine.createEntity();
-	engine.getComponentManager().createBundle(EngineComponents::CameraBundle_v,
-		e, {}, {});
-	auto cb = engine.getComponentManager().createBundle(EngineComponents::CameraBundle_v,
-		e);
-
-	engine.run();*/
 
 	return 0;
 }
